0408db61719095e61a373dda547784886bc4a1da
Svitlana Dyachok <korneyswet@gmail.com>
2023-03-19 12:55:04 +0200

    new

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+const STORAGE_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onSaveTimePlayer, 1000));
+
+function onSaveTimePlayer(evt) {
+  const playerTime = evt.seconds;
+  localStorage.setItem(STORAGE_KEY, playerTime);
+}
+const savedTimeVideo = localStorage.getItem(STORAGE_KEY);
+
+if (savedTimeVideo) {
+  player.setCurrentTime(savedTimeVideo);
+}
